97f30cd85101b2de2e92ab169f64df80a41a849d
Judit <j.aizpuru@campus.tu-berlin.de>
2025-10-07 21:10:59 +0200

    wrong jobspy detected with pipreqs -

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import sys
 from pathlib import Path
 
-print(f"Path for importing modules to tests: {str(Path(__file__).resolve().parent.parent)}")
+print(f"Add root path for importing modules to tests: {str(Path(__file__).resolve().parent.parent)}")
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
